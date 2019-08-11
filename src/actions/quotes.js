44d36670875ea86bd0bddb80b86b6a1da5ba63c0
bojosteph <bojosteph@gmail.com>
2019-08-10 18:35:05 -0700

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,36 @@
 // TODO: Create action creators as defined in tests
+import {
+  ADD_QUOTE,
+  REMOVE_QUOTE,
+  UPVOTE_QUOTE,
+  DOWNVOTE_QUOTE
+
+} from './types';
+
+export const addQuote = quote => {
+  return {
+    type: ADD_QUOTE,
+     quote
+  }
+}
+
+export const removeQuote = quoteId => {
+  return {
+    type: REMOVE_QUOTE,
+     quoteId
+  }
+}
+
+export const upvoteQuote = quoteId => {
+  return {
+    type: UPVOTE_QUOTE,
+     quoteId
+  }
+}
+
+export const downvoteQuote = quoteId => {
+  return {
+    type: DOWNVOTE_QUOTE,
+     quoteId
+  }
+}
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,31 @@
+import {
+  ADD_QUOTE,
+  REMOVE_QUOTE,
+  UPVOTE_QUOTE,
+  DOWNVOTE_QUOTE
+
+} from '../actions/types';
+
 export default (state = [], action) => {
-  return state;
+  switch(action.type) {
+    case ADD_QUOTE:
+      return [...state, action.quote]
+    case REMOVE_QUOTE:
+      return state.filter(quote => quote.id !== action.quoteId)
+    case UPVOTE_QUOTE:
+      const index = state.findIndex(quote => quote.id === action.quoteId)
+      const quoteForUpVote = {...state[index]}
+      const upvotedQuote = {...quoteForUpVote, votes: quoteForUpVote.votes += 1 }
+      return [...state.slice(0, index), upvotedQuote, ...state.slice(index + 1)]
+    case DOWNVOTE_QUOTE:
+      const downIndex = state.findIndex(quote => quote.id === action.quoteId)
+      const quoteForDownVote = {...state[downIndex]}
+      const downvotedQuote = {...quoteForDownVote, votes: quoteForDownVote.votes -= 1}
+      if (quoteForDownVote.votes > 0) {  
+        return [...state.slice(0, downIndex), downvotedQuote, ...state.slice(downIndex + 1)]
+      }
+
+    default:
+      return state;
+  }
 }
